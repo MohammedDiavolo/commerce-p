80ccaea5b339ed12457779ebb1d34a98ee81f87e
mohamedelsayedsh <mo7amed.elsayedsh7@gmail.com>
2024-09-03 15:18:09 +0300

    Add the auth folder and its components.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ProductsDetailsComponent } from './products/components/products-details/products-details.component';
 import { CartComponent } from './carts/components/cart/cart.component';
 import { AllProductsComponent } from './products/components/all-products/all-products.component';
+import { LoginComponent } from './auth/components/login/login/login.component';
 
 export const routes: Routes = [
   {
@@ -18,5 +19,10 @@ export const routes: Routes = [
     path:'cart',
     component: CartComponent,
     title: 'Cart-page'
+  },
+  {
+    path:'login',
+    component:LoginComponent,
+    title:'Login-page'
   }
 ];
--- src/app/products/components/all-products/all-products.component.ts
+++ src/app/products/components/all-products/all-products.component.ts
@@ -63,4 +63,6 @@ export class AllProductsComponent implements OnInit {
     );
   }
 
+  
+
 }
