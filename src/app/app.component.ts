6dd1b51cfb7e128702ee7d0aab2f1a95e1ae765a
mohamedelsayedsh <mo7amed.elsayedsh7@gmail.com>
2024-09-06 21:35:00 +0300

    edit on cart page and add login and signup page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,39 @@
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
 import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,FooterComponent,HttpClientModule,FormsModule],
+  imports: [RouterOutlet, HeaderComponent,FooterComponent,HttpClientModule,FormsModule,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 
 export class AppComponent {
   title = 'FutureTech';
+  showHeaderFooter: boolean = true;
+  showLayout: boolean = true;
+
+    constructor(private router: Router) {
+      this.router.events.subscribe((event: any) => {
+        if (event.url === '/login' || event.url ==='/signup') {
+          this.showHeaderFooter = false;
+        } else {
+          this.showHeaderFooter = true;
+        }
+      });
+      this.router.events.subscribe((event: any) => {
+        if (event.url === '/login'|| event.url === '/cart'|| event.url ==='/signup') {
+          this.showLayout = false;
+        } else {
+          this.showLayout = true;
+        }
+      });
+    }
 }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,26 +3,32 @@ import { ProductsDetailsComponent } from './products/components/products-details
 import { CartComponent } from './carts/components/cart/cart.component';
 import { AllProductsComponent } from './products/components/all-products/all-products.component';
 import { LoginComponent } from './auth/components/login/login/login.component';
+import { SignupComponent } from './auth/components/signup/signup/signup.component';
 
 export const routes: Routes = [
   {
-    path:'',
+    path: '',
     component: AllProductsComponent,
     title: 'Product-Page'
   },
   {
-    path:'details/:id',
+    path: 'details/:id',
     component: ProductsDetailsComponent,
     title: 'Product-Details-Page'
   },
   {
-    path:'cart',
+    path: 'cart',
     component: CartComponent,
     title: 'Cart-page'
   },
   {
-    path:'login',
-    component:LoginComponent,
-    title:'Login-page'
+    path: 'login',
+    component: LoginComponent,
+    title: 'Login-page'
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+    title: 'Signup-page'
   }
 ];
